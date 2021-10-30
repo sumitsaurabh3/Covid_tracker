e8517efb968733ac952c6653de2a3a378a4d2fed
Sumit <sumitsaurabh799@gmail.com>
2021-10-30 13:01:12 +0530

    CovPass

---
+++ app/src/main/java/com/example/covpass/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.covpass;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_scan;
+    private ImageView imageView;
+    private TextView textView,TextView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_scan = (Button) findViewById(R.id.btn_scan);
+        btn_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity2();
+
+            }
+        });
+    }
+
+    public void openMainActivity2() {
+        Intent intent = new Intent(this,  MainActivity2.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/covpass/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.covpass;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+}
