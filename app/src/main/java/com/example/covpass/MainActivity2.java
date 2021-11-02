33317d880eb1f619ec9ae70990a4b689f0909a6f
Sumit <sumitsaurabh799@gmail.com>
2021-11-02 18:59:35 +0530

    Covid Cerficate Verify

--- app/src/main/java/com/example/covpass/MainActivity2.java
+++ app/src/main/java/com/example/covpass/MainActivity2.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
-
+//Initialise variable
+private Button btnScanBarcode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        btnScanBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity2.this, ScanBarCodeActivity.class));
+            }
+        });
     }
 }
